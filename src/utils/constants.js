c44cdbe581fbfa32e144d61a60c9860f7ace2844
DmitryDoroshko <dmitry.doroshko.99@gmail.com>
2022-08-03 12:02:25 +0300

    Added API constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+export const FIREBASE_API_URL_MINIFIED = "https://dimitri-food-order-app-default-rtdb.firebaseio.com/";
+export const FIREBASE_API_URL_MEALS = "https://dimitri-food-order-app-default-rtdb.firebaseio.com/meals.json";
+export const FIREBASE_API_URL_ORDERS = "https://dimitri-food-order-app-default-rtdb.firebaseio.com/meals.json";
